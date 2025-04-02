571a5ea10a1ffab2c29b5b6f90c6bc7290eb6144
Alexey Chechetkin <chass.99@mail.ru>
2025-04-02 20:33:28 +0300

    2.0

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
